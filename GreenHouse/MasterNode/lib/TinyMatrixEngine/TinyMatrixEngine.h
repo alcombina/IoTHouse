77fb22a4ce35affa5c4c6511fe1b80b5b8b54756
Alberto Combina <combina.alberto@gmail.com>
2026-05-04 21:50:03 +0200

    Simplified state printing

--- GreenHouse/MasterNode/lib/TinyMatrixEngine/TinyMatrixEngine.h
+++ GreenHouse/MasterNode/lib/TinyMatrixEngine/TinyMatrixEngine.h
@@ -1,17 +1,30 @@
 #pragma once
-
 #include <Arduino_LED_Matrix.h>
 
 class TinyMatrixEngine {
 private:
     ArduinoLEDMatrix __matrix;
-    uint32_t __frame[3] = {0x80080080,0x08008008,0x00800800}; 
+    const uint32_t __st[3]   = {0xe0, 0x880ec02,0x80e80000};
+    uint32_t __N [10][3] =  {
+    { 0x0, 0xe00a00a0, 0xa00e000 }, // 0
+	{ 0x0, 0x400c0040, 0x400e000 }, // 1
+	{ 0x0, 0xc0020040, 0x800e000 }, // 2
+	{ 0x0, 0xe0020060, 0x200e000 }, // 3
+	{ 0x0, 0xa00a00e0, 0x2002000 }, // 4
+	{ 0x0, 0xe00800e0, 0x200e000 }, // 5
+	{ 0x0, 0xe00800e0, 0xa00e000 }, // 6
+	{ 0x0, 0xe0020020, 0x2002000 }, // 7
+	{ 0x0, 0xe00a00e0, 0xa00e000 }, // 8
+	{ 0x0, 0xe00a00e0, 0x200e000 }  // 9
+};
+
+
 
 public:
+
     TinyMatrixEngine();
 
     void begin();
-    void show();
-    void shiftFrameRight();
-    void getFrameBitmap(bool);
+    void clearFrame();
+    void setState(int index);
 };
--- GreenHouse/MasterNode/src/main.cpp
+++ GreenHouse/MasterNode/src/main.cpp
@@ -3,14 +3,20 @@
 
 
 TinyMatrixEngine ledMatrix;
+int num = 0;
 
 void setup() {
   Serial.begin(9600);
   ledMatrix.begin();
-  ledMatrix.getFrameBitmap(true);
+  ledMatrix.setState(0);
 }
 
 void loop() {
-  // Serial.println("Ciao");
-  delay(5000);
+  int newNum = (millis() / 1000) % 10; // Display numbers from 0 to 9 in a loop every second
+  if (newNum != num) {
+    num = newNum;
+    ledMatrix.setState(num);
+    Serial.print("Current number: ");
+    Serial.println(newNum);
+  }
 }
