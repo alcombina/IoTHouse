c3703489b8fd007aaa9d7fee048deef50bb0b3d0
Alberto Combina <combina.alberto@gmail.com>
2026-05-10 19:06:33 +0200

    Added state machine and wifi connection

--- GreenHouse/MasterNode/src/main.cpp
+++ GreenHouse/MasterNode/src/main.cpp
@@ -1,35 +1,76 @@
 #include <Arduino.h>
+#include "layout.h"
+#include "secrets.h"
 #include "TinyMatrixEngine.h"
 #include "DHTSensor.h"
-#include "layout.h"
+#include "WiFiS3.h"
 
 // DHT Sensor setup
-DHTSensor dhtSensor(DHT_DATA_PIN, DHT22, 2000, true);
+DHTSensor dhtSensor(DHT_DATA_PIN, DHT22, 4000, true); // Pin, Type, Frequency, Verbose
 
+//TinyMatrixEngine setup
 TinyMatrixEngine ledMatrix;
-int num = 0;
+
+// WiFi setup
+#define POST_INTERVAL 10000 // Time between POST requests in milliseconds
+unsigned long lastPostTime = 0;
+WiFiClient client;
+
+// State machine
+enum states {
+  INIT,
+  RUNNING,
+  ERROR
+};
+
+states state;
+const long initRetryMillis = 3000;
 
 void setup() {
+    // Initialize components
+  state = INIT;
   Serial.begin(9600);
 
+  // Initialize LED Matrix
   ledMatrix.begin();
-  ledMatrix.setState(0);
+  ledMatrix.setState(INIT);
 
+  // Initialize DHT Sensor
   dhtSensor.begin();
 }
 
 void loop() {
-  int newNum = (millis() / 1000) % 10; // Display numbers from 0 to 9 in a loop every second
-  if (newNum != num) {
-    num = newNum;
-    ledMatrix.setState(num);
-    // Serial.print("Current number: ");
-    // Serial.println(newNum);
-  }
+  static unsigned long lastClock = -initRetryMillis; // Force immediate connection attempt on startup
 
-  // Read DHT sensor data
-  static float temperature = 0;
-  static float humidity = 0;
-  temperature = dhtSensor.readTemperature();
-  humidity = dhtSensor.readHumidity();
+  switch (state)
+  {
+  case INIT:
+    if (millis() - lastClock < initRetryMillis) {
+      break; // Wait for 3 seconds before trying to connect again
+    }
+
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Connecting to WiFi...");
+      WiFi.begin(WIFI_NETWORK_NAME, WIFI_PASSWORD);
+    } else {
+      Serial.println("\nConnected to WiFi!");
+      state = RUNNING;
+      ledMatrix.setState(RUNNING);
+    }
+
+    break;
+  case RUNNING:
+    // Read DHT sensor data
+    static float temperature = 0;
+    static float humidity = 0;
+    temperature = dhtSensor.readTemperature();
+    humidity = dhtSensor.readHumidity();
+    break;
+  case ERROR:
+    // Error handling code
+    break;
+
+  default:
+    break;
+  }
 }
